57c3aa25951f35585ce981e6cab3fe052c9e245f
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2024-04-23 11:59:08 +0200

    cleaned up code for plotting

--- versuch1/sinus.m
+++ versuch1/sinus.m
@@ -1,19 +1,21 @@
 clear
 
 % x-Axis
-time = linspace(0, 3e-3, 10e3);  % seconds
+time = linspace(0, 3e-3);
 
-% declare sine function
-function f1 = myfun(amplitude, frequency, time, offset)
-    f1 = amplitude * sin(2 * pi * frequency * time - offset);
+% declare functions
+function f1 = sine1(time)
+    f1 = 2 * sin(2 * pi * 2e3 * time - 0);
+end
+function f2 = sine2(time)
+    f2 = 4 * sin(2 * pi * 6e3 * time - pi./4);
 end
 
-
-f1 = myfun(2, 2e3, time, 0);
-f2 = myfun(4, 6e3, time, -pi./4);
+f1 = sine1(time);
+f2 = sine2(time);
 f3 = f1 .* f1;
 
-% plotting
+% plot functions
 sinplots = tiledlayout(4,1);
 nexttile
 plot(time, f1)
@@ -35,20 +37,25 @@ title('Lissajous-Figur')
 
 exportgraphics(sinplots, "sinus.pdf", 'ContentType','vector')
 
-% task d
+% plot with wrong paramters
 figure
-t=linspace(0.3,1e3);
 lissplots = tiledlayout(2,1);
+time = linspace(0, 3, 1e3);
+
 nexttile
-plot(myfun(2, 2e3, t, 0), myfun(2, 6e3, t, -pi./4))
+f1 = sine1(time);
+f2 = sine2(time);
+plot(f1, f2)
 xlabel('x1(t)')
 ylabel('x2(t)')
 title('Zeitbereich 0:3 s, 1000 Intervalle')
 
-% task e
-t=linspace(0.3,1e3+1);
+% another set of wrong paramters
+time = linspace(0, 3, 1e3+1);
 nexttile
-plot(myfun(2, 2e3, t, 0), myfun(2, 6e3, t, -pi./4))
+f1 = sine1(time);
+f2 = sine2(time);
+plot(f1, f2)
 xlabel('x1(t)')
 ylabel('x2(t)')
 title('Zeitbereich 0:3 s, 1001 Intervalle')
