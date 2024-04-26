cd55541ee7c74e1d6b86033ece763546824a3ec8
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2024-04-26 15:11:10 +0200

    try to combine plots

--- versuch1/temp_regler.m
+++ versuch1/temp_regler.m
@@ -1,17 +1,44 @@
+% global parameters
 LOCAL_DIRECTORY = "C:\Users\janho\Coding\Regelungstechnik\versuch1\";
+STOPTIME = 70*60 - 1;
+POINTS = 1e2;
 
-param_k1 = 10/400;
-param_k2 = 400/230;
-
-param_time_const = 15*60;
-
-param_switch_on = 0.2;
-param_switch_off = -0.2;
-param_switch_maxv = 230;
+STEPSIZE = STOPTIME / POINTS;
 
 
-% sim(LOCAL_DIRECTORY + 'temp_regler.slx')
-% mdl = "IntegrateSine";
-% open("mdl")
-
+% set model parameters
 model = LOCAL_DIRECTORY +  'tempregler_modell.slx';
+k1 = 10/400;
+k2 = 400/230;
+time_const = 15*60;
+
+% run first simulation
+controlller_on = 0.2;
+controller_off = -0.2;
+output = sim(model, "StopTime", num2str(STOPTIME), 'FixedStep', num2str(STEPSIZE));  % 1: control variable, 2: manipulated_variable, 3: reference_variable
+
+% second simulation with changed parameters
+controlller_on = 0.01;
+controller_off = -0.01;
+output_new = sim(model, "StopTime", num2str(STOPTIME), 'FixedStep', num2str(STEPSIZE));  % 1: control variable, 2: manipulated_variable, 3: reference_variable
+
+figure;
+subplot(1,2,1)      % rows, columns, position
+outputplot = tiledlayout(3,1);
+title(outputplot, "alte controller werte")
+nexttile
+plot(output.yout{1}.Values.Time, output.yout{1}.Values.Data)
+nexttile
+plot(output.yout{2}.Values.Time, output.yout{2}.Values.Data)
+nexttile
+plot(output.yout{2}.Values.Time, output.yout{3}.Values.Data)
+
+subplot(1,2,2)      % rows, columns, position
+outputplot_new = tiledlayout(3,1);
+nexttile
+plot(output_new.yout{1}.Values.Time, output_new.yout{1}.Values.Data)
+nexttile
+plot(output_new.yout{2}.Values.Time, output_new.yout{2}.Values.Data)
+nexttile
+plot(output_new.yout{2}.Values.Time, output_new.yout{3}.Values.Data)
+title(outputplot_new, "neue controller werte")
