7f2c1939e0c99ee8198b338f832a789aca5fbd39
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2024-05-10 12:03:26 +0200

    fit error margin to the x scale

--- versuch2/kessel.m
+++ versuch2/kessel.m
@@ -9,15 +9,15 @@ SURFACE_AREA = pi*(18e-2)^2;
 KR_LIST = [0.01,0.005,0.001,0.0005,0.0001];
 STOPTIME_LIST = [80, 150, 600, 1500, 6000];
 
-ERROR_MARGIN = 0.00001;
+ERROR_MARGIN_LIST = [1e-5, 1e-5, 1e-6, 1e-6, 1e-7];
 
 %___________________________________________________________________
 % load model
 model = 'kessel_modell';
 load_system(model);
 %     'MaxStep', num2str(STEPSIZE), ...
-% "SolverType","Variable-step", ...
-%     "SolverName", "VariableStepAuto", ...
+
+    
 
 %___________________________________________________________________
 % run simulation 
@@ -25,21 +25,24 @@ figure
 kessel_plot = tiledlayout('flow');
 
 % for i = 1:length(KR_LIST)
-for i = 1:1
+for i = 1:5
     set_param(model,...
-        'StopTime', num2str(STOPTIME_LIST(i)))
+        'StopTime', num2str(STOPTIME_LIST(i)), ...
+        "SolverType","Variable-step", ...
+        "SolverName", "VariableStepAuto" ...
+    )
 
     KR = KR_LIST(i);
     output = sim(model);
 
     % find the time constant value
-    max_value = output.yout{3}.Values.Data(1)
-    q_t = max_value * (1 - 0.632)
-    % q_time_const = 1- (max_value * 0.632)
+    ERROR_MARGIN = ERROR_MARGIN_LIST(i);
+    max_value = output.yout{3}.Values.Data(1);
+    q_t = max_value * (1 - 0.632);
     index_list = find(abs(output.yout{3}.Values.Data - q_t) < ERROR_MARGIN);
-    t_t_index = index_list(1)
-    t_t = output.tout(t_t_index)
-    % print()
+    t_t_index = index_list(1);
+    t_t = output.yout{3}.Values.Time(t_t_index);
+    disp(fprintf('KR = %.4f \t Tau = %.4f', KR, t_t))
 
     % plotting
     nexttile
@@ -51,14 +54,14 @@ for i = 1:1
         plot(output.tout, output.yout{2}.Values.Data);
         hold on
     yyaxis right
-        plot(output.tout, output.yout{3}.Values.Data);
+        plot(output.yout{3}.Values.Time, output.yout{3}.Values.Data);
         ylabel('Durchfluss in m^3/s')
         title(sprintf('KR = %.4f', KR))
         xlabel('Zeit in s')
 
         %plot time constant
-        xline(t_t)
-        % yline(max_value * (1 - 0.632))
+        xline(t_t, ':', 'Time constant')
+        yline(q_t)
 end
 
 lgd = legend('Führungsgröße', 'Stellgröße', 'Regelgröße');
